a686cc22b6d946c372cff58640be5d063c62512f
saei1d <hehehe3bar@gmail.com>
2023-10-31 04:20:11 +0330

    mmd

--- client/urls.py
+++ client/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from client.views import *
 
 urlpatterns = [
-    path('', Name.as_view())
+    path('', Namee.as_view())
 ]
--- client/views.py
+++ client/views.py
@@ -8,16 +8,15 @@ from rest_framework.views import APIView
 from client.models import Name
 
 
-class Name(APIView):
+class Namee(APIView):
     permission_classes = (
         permissions.AllowAny,
-        #                              ^
-        #                         a comma here
     )
 
     def post(self, request):
         context = {}
         try:
+
             salam = request.POST.get('name')
             mmd = Name(name=salam)
             mmd.save()
@@ -30,4 +29,12 @@ class Name(APIView):
 
     def get(self, request):
         context = {}
-        return HttpResponse('salaaaaaaaaaaaaaaaaaaaaaaaaam')
+        try:
+            mmd = Name.objects.all()
+            context['mmd'] = mmd.name
+            context['msg'] = "ذخیره گردید"
+            status_code = HTTP_200_OK
+        except:
+            context['msg'] = 'Not Found'
+            status_code = HTTP_404_NOT_FOUND
+        return Response(context, status=status_code)
